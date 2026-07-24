e76dd3a8073b90ec4019a29ad72f9f093b9bfea0
sterlingwebsite <ssteele1@byupathway.edu>
2026-07-24 16:33:58 -0600

    fixing render

--- swagger.js
+++ swagger.js
@@ -1,6 +1,7 @@
 const swaggerAutogen = require("swagger-autogen")();
+const os = require("os");
 
-const isProduction = process.env.RENDER === "true";
+const isProduction = os.platform() === "linux";
 
 const doc = {
   info: {
@@ -8,14 +9,11 @@ const doc = {
     description:
       "API documentation for managing patients, doctors, and appointments.",
   },
-  host: isProduction
-    ? "cse341finalproject-mn1w.onrender.com"
-    : "localhost:3000",
+  host: isProduction ? "://onrender.com" : "localhost:3000",
   schemes: isProduction ? ["https"] : ["http"],
 };
 
 const outputFile = "./swagger.json";
-
 const endpointsFiles = ["./index.js"];
 
 swaggerAutogen(outputFile, endpointsFiles);
