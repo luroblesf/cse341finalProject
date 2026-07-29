fdcdd3aa9b754fc4cc689f96c0aa5e216484a530
sterlingwebsite <ssteele1@byupathway.edu>
2026-07-29 17:13:43 -0600

    added authorization. right now it requires typing into the url. still working on making authorization links functional

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("./config/passport");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "temporary-local-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(
   "/api-docs",
@@ -24,6 +37,8 @@ app.use(
 
 app.use("/patients", require("./routes/patients"));
 app.use("/doctors", require("./routes/doctors"));
+app.use("/auth", require("./routes/auth"));
+app.use("/users", require("./routes/users"));
 
 mongoose
   .connect(process.env.MONGODB_URI)
--- swagger.js
+++ swagger.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const swaggerAutogen = require("swagger-autogen")();
 const os = require("os");
 
@@ -6,13 +7,33 @@ const isProduction = os.platform() === "linux";
 const doc = {
   info: {
     title: "Clinic Management API",
-    description:
-      "API documentation for managing patients, doctors, and appointments.",
+    description: "API Authentication Hub",
   },
+
+  externalDocs: {
+    description: `
+    [Login with GitHub](http://localhost:3000/auth/github)
+
+    [Logout](http://localhost:3000/auth/logout)
+        `,
+        url: "http://localhost:3000",
+  },
+
   host: isProduction
     ? "cse341finalproject-mn1w.onrender.com"
     : "localhost:3000",
   schemes: isProduction ? ["https"] : ["http"],
+  securityDefinitions: {
+    githubOAuth: {
+      type: "oauth2",
+      authorizationUrl: isProduction
+        ? "https://onrender.com"
+        : "http://localhost:3000/auth/github",
+      flow: "implicit",
+      description:
+        "Log in via GitHub OAuth to access protected POST and PUT endpoints.",
+    },
+  },
 };
 
 const outputFile = "./swagger.json";
